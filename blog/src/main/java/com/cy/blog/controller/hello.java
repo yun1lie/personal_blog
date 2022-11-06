236e305e513cae04bb194a147162fcef2dffda5c
littleJ <1446839626@qq.com>
2022-11-06 21:20:55 +0800

    做梦的一天  Article搞定

--- blog/src/main/java/com/cy/blog/controller/hello.java
+++ blog/src/main/java/com/cy/blog/controller/hello.java
@@ -65,15 +65,24 @@ public class hello {
         List<Article> allArticleList = mapper.article();
         model.addAttribute("allArticle",allArticleList);
 
-        List<Article> previousArticleList = mapper.getPreviousArticle(article);
-        Article previousArticle = previousArticleList.get(0);
-        model.addAttribute("previousArticle",previousArticle);
-
-        List<Article> nextArticleList = mapper.getNextArticles(article);
-        Article nextArticle = nextArticleList.get(0);
-        model.addAttribute("nextArticle",nextArticle);
-
         try {
+
+            List<Article> previousArticleList = mapper.getPreviousArticle(article);
+            if (previousArticleList.size() > 0){
+                Article previousArticle = previousArticleList.get(0);
+                model.addAttribute("previousArticle",previousArticle);
+
+            }else {
+                Article previousArticle = new Article();
+                model.addAttribute("previousArticle",previousArticle);
+            }
+
+
+
+            List<Article> nextArticleList = mapper.getNextArticles(article);
+            Article nextArticle = nextArticleList.get(0);
+            model.addAttribute("nextArticle",nextArticle);
+
             List<Article> articleList = mapper.getArticle(article);
             Article newArticle = articleList.get(0);
             model.addAttribute("newArticle", newArticle);
